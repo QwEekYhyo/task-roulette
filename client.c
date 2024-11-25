877a4dd1d04592dab2ad47451b06de258cff19f4
Logan Lucas <logan.lucas83@gmail.com>
2024-11-25 13:10:24 +0100

    rename client and server source file

--- client.c
+++ client.c
@@ -4,24 +4,20 @@
 #include <string.h>
 
 #define ADDRESS "tcp://localhost:1883"
-#define CLIENTID "TaskRouletteServer"
+#define CLIENTID "TaskRouletteClient"
 #define TOPIC "game/request"
+#define PAYLOAD "Hello World!"
 #define QOS 1
 #define TIMEOUT 10000L
-
-volatile MQTTClient_deliveryToken deliveredtoken;
-
-void delivered(void* context, MQTTClient_deliveryToken dt) {
-    printf("Message with token value %d delivery confirmed\n", dt);
-    deliveredtoken = dt;
-}
 
 int message_arrived(void* context, char* topicName, int topicLen, MQTTClient_message* message) {
     int i;
     char* payloadptr;
+
     printf("Message arrived\n");
     printf("     topic: %s\n", topicName);
     printf("   message: ");
+
     payloadptr = message->payload;
     for (i = 0; i < message->payloadlen; i++) {
         putchar(*payloadptr++);
@@ -29,6 +25,7 @@ int message_arrived(void* context, char* topicName, int topicLen, MQTTClient_mes
     putchar('\n');
     MQTTClient_freeMessage(&message);
     MQTTClient_free(topicName);
+
     return 1;
 }
 
@@ -40,28 +37,29 @@ void connection_lost(void* context, char* cause) {
 int main(int argc, char* argv[]) {
     MQTTClient client;
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    int rc;
+    MQTTClient_deliveryToken token;
     MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
-    MQTTClient_setCallbacks(client, NULL, connection_lost, message_arrived, delivered);
+    MQTTClient_setCallbacks(client, NULL, connection_lost, message_arrived, NULL);
+
+    int rc;
     if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
         printf("Failed to connect, return code %d\n", rc);
         exit(EXIT_FAILURE);
     }
-    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
-           "Press Q<Enter> to quit\n\n",
-           TOPIC, CLIENTID, QOS);
-    MQTTClient_subscribe(client, TOPIC, QOS);
 
-    int ch;
-    while (1) {
-        ch = getchar();
-        if (ch == 'Q' || ch == 'q') break;
-    }
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    pubmsg.payload = PAYLOAD;
+    pubmsg.payloadlen = strlen(PAYLOAD);
+    pubmsg.qos = QOS;
+    pubmsg.retained = 0;
+    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+    printf("Waiting for publication of %s\n"
+           "on topic %s for client with ClientID: %s\n",
+           PAYLOAD, TOPIC, CLIENTID);
 
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
-
     return rc;
 }
--- server.c
+++ server.c
@@ -4,27 +4,17 @@
 #include <string.h>
 
 #define ADDRESS "tcp://localhost:1883"
-#define CLIENTID "TaskRouletteClient"
+#define CLIENTID "TaskRouletteServer"
 #define TOPIC "game/request"
-#define PAYLOAD "Hello World!"
 #define QOS 1
 #define TIMEOUT 10000L
-
-volatile MQTTClient_deliveryToken deliveredtoken;
-
-void delivered(void* context, MQTTClient_deliveryToken dt) {
-    printf("Message with token value %d delivery confirmed\n", dt);
-    deliveredtoken = dt;
-}
 
 int message_arrived(void* context, char* topicName, int topicLen, MQTTClient_message* message) {
     int i;
     char* payloadptr;
-
     printf("Message arrived\n");
     printf("     topic: %s\n", topicName);
     printf("   message: ");
-
     payloadptr = message->payload;
     for (i = 0; i < message->payloadlen; i++) {
         putchar(*payloadptr++);
@@ -32,7 +22,6 @@ int message_arrived(void* context, char* topicName, int topicLen, MQTTClient_mes
     putchar('\n');
     MQTTClient_freeMessage(&message);
     MQTTClient_free(topicName);
-
     return 1;
 }
 
@@ -44,31 +33,28 @@ void connection_lost(void* context, char* cause) {
 int main(int argc, char* argv[]) {
     MQTTClient client;
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    MQTTClient_deliveryToken token;
+    int rc;
     MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
-    MQTTClient_setCallbacks(client, NULL, connection_lost, message_arrived, delivered);
-
-    int rc;
+    MQTTClient_setCallbacks(client, NULL, connection_lost, message_arrived, NULL);
     if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
         printf("Failed to connect, return code %d\n", rc);
         exit(EXIT_FAILURE);
     }
+    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
+           "Press Q<Enter> to quit\n\n",
+           TOPIC, CLIENTID, QOS);
+    MQTTClient_subscribe(client, TOPIC, QOS);
 
-    MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    pubmsg.payload = PAYLOAD;
-    pubmsg.payloadlen = strlen(PAYLOAD);
-    pubmsg.qos = QOS;
-    pubmsg.retained = 0;
-    deliveredtoken = 0;
-    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
-    printf("Waiting for publication of %s\n"
-           "on topic %s for client with ClientID: %s\n",
-           PAYLOAD, TOPIC, CLIENTID);
-    while (deliveredtoken != token);
+    int ch;
+    while (1) {
+        ch = getchar();
+        if (ch == 'Q' || ch == 'q') break;
+    }
 
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
+
     return rc;
 }
