a2ebc194f2b0888daee75d4fa5f776242cee2dbe
Logan Lucas <logan.lucas83@gmail.com>
2024-11-26 13:02:24 +0100

    function to get PID of nth process running on computer

--- include/utils.h
+++ include/utils.h
@@ -2,11 +2,14 @@
 #define TROULETTE_UTILS_H
 
 #include <stdint.h>
+#include <time.h>
 
 static const uint8_t UUID_STR_LEN = 24;
 
 extern const char HEX_CHARS[];
 
 void generate_uuid(char buffer[UUID_STR_LEN]);
+
+pid_t get_nth_process(unsigned int n);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,10 @@
 #include <utils.h>
 
-#include <stdlib.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 const char HEX_CHARS[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
@@ -9,4 +12,25 @@ void generate_uuid(char buffer[UUID_STR_LEN]) {
     for(uint8_t i = 0; i < UUID_STR_LEN; i++) {
         buffer[i] = HEX_CHARS[rand() % 16];
     }
+}
+
+pid_t get_nth_process(unsigned int n) {
+    DIR *proc_directory = opendir("/proc");
+    if (!proc_directory) {
+        perror("opendir");
+        return -1;
+    }
+
+    unsigned int count = 0;
+    struct dirent* result;
+    struct dirent* entry;
+    while ((entry = readdir(proc_directory)) != NULL && count < n) {
+        if (isdigit(entry->d_name[0])) {
+            result = entry;
+            count++;
+        }
+    }
+
+    if (!result) return -1;
+    return atoi(result->d_name);
 }
